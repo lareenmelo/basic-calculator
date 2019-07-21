2a9e21780c3c529a236fb1d41f3ece40fda1f294
Lareen Melo <lareenmelo@gmail.com>
2019-07-20 23:31:50 -0400

    Adding Unit Tests target

---
+++ basic-calculator-tests/basic_calculator_tests.swift
@@ -0,0 +1,33 @@
+//
+//  basic_calculator_tests.swift
+//  basic-calculator-tests
+//
+//  Created by Lareen Melo on 7/20/19.
+//  Copyright © 2019 Lareen Melo. All rights reserved.
+//
+
+import XCTest
+
+class basic_calculator_tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
