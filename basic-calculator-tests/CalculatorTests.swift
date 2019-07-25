23b6a49aa2391bf34d7dde56abb5c45a5091e3ac
lareenmelo <lareenmelo@gmail.com>
2019-07-25 12:32:14 -0400

    Adding tearDown functionalities

--- basic-calculator-tests/CalculatorTests.swift
+++ basic-calculator-tests/CalculatorTests.swift
@@ -19,6 +19,7 @@ class CalculatorTests: XCTestCase {
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        calculator.clearHistory()
     }
 
     
