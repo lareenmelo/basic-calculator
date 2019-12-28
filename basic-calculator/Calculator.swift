6d1fad15fe857303057d57bad6d788281cd2373d
Lareen Melo <lareenmelo@gmail.com>
2019-12-28 16:48:29 -0500

    Fix #2

--- basic-calculator/Calculator.swift
+++ basic-calculator/Calculator.swift
@@ -93,10 +93,10 @@ class Calculator {
                 if let number = accumulator.toInt() {
                     return String(number)
                 } else {
-                    return String(accumulator.round(withPrecision: 8))
+                    return String(accumulator)
                 }
             } else {
-                return String(accumulator.round(withPrecision: 8))
+                return String(accumulator)
             }
         }
     }
@@ -104,11 +104,6 @@ class Calculator {
 
 
 extension Double {
-    func round(withPrecision precision: Int) -> Double {
-        let divisor = pow(10.0, Double(precision))
-        return (self * divisor).rounded() / divisor
-    }
-    
     func toInt() -> Int? {
         if self >= Double(Int.min) && self <= Double(Int.max) {
             return Int(self)
